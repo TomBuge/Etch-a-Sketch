cb786b4b9da7b7fc306ca3716aa4844f371581d8
Tom Bugeja <46567954+TomBuge@users.noreply.github.com.>
2025-12-01 11:47:55 +0000

    Add drawing functionality
    add makeGrids() and addGridColor() to allow
    drawing and button so user can change size of
    grids.

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
 
 const container = document.querySelector(".container");
-let i = 0; 
-while (i < 256) {
-    const div = document.createElement("div");
-    div.id = i;
-    div.style.width = "6.25%"
-    div.style.height = "100px"
-    div.style.border = "1px solid blue";
-    container.appendChild(div);
-    i++;
+
+
+function makeGrids(num) {
+    container.textContent = "";
+    let divWidth = 100 / num;
+
+    let i = 0; 
+    while (i < num * num) {
+        const div = document.createElement("div");
+        div.id = i;
+        div.classList.add("grid");
+        div.style.width = `${divWidth}%`;
+        div.style.aspectRatio = "1 / 1";
+        // div.style.border = "1px solid blue";
+        container.appendChild(div);
+        i++;
+    }
 }
 
+function addGridColor()  {
 
-/* 
-create div element and asign variable 
-assign div css style of flex: 1;
-assign id equal to i 
-append div to container 
-*/
+    const grids = document.querySelectorAll(".grid");
+    
+
+    grids.forEach(grid => {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        const rgb = `rgb(${r}, ${g}, ${b})`;
+        grid.addEventListener("mouseenter", (e) => {
+            grid.style.background = rgb;
+        })
+        
+    });
+}
+
+makeGrids(16);
+addGridColor();
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    let num = prompt("Choose a number between 4 - 100 for pen thickness");
+    if (num > 100 || num < 4) {
+        num = prompt("Please enter a valid number between 4 and 100");
+    }
+    makeGrids(num);
+    addGridColor();  
+});
+
